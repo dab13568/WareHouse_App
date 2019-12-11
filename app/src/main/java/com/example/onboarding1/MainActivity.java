496199819b0b01c72922e4e959f82c3154bb65d4
shlomichi1351 <shlomichi1351gmail.com>
2019-12-11 15:23:32 +0200

    firebase.

--- app/src/main/java/com/example/onboarding1/MainActivity.java
+++ app/src/main/java/com/example/onboarding1/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.onboarding1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ImageButton imageButton;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageButton=findViewById(R.id.add_package);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this, AddPackage.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
+
 }
--- app/src/main/java/com/example/onboarding1/delivered_activity.java
+++ app/src/main/java/com/example/onboarding1/delivered_activity.java
@@ -25,7 +25,7 @@ public class delivered_activity extends AppCompatActivity {
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
-                Intent intent =new Intent(delivered_activity.this, AddPackage.class);
+                Intent intent =new Intent(delivered_activity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
